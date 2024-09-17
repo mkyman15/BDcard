5f6a3938fe188244259531992801b8a10829dcb0
mkyman15 <quinn.delacruz@gmail.com>
2024-09-16 17:41:57 -0700

    new stuff

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import Card from "./Card";
+import "./styles.css";
+
+export default function App() {
+  var button = document.getElementById("root");
+
+  // Define a function to be executed when the button is clicked
+  function handleClick() {
+    var newDiv = document.createElement("div");
+
+    // 2. Set attributes or styles
+    newDiv.id = "myNewDiv"; // Set an ID for the <div>
+    newDiv.className = "myClass"; // Set a class name
+    newDiv.backgroundImage = "ballons.png"; // Set inline styles
+
+    // 3. Add content to the <div>
+    newDiv.innerHTML = ""; // Add HTML content
+
+    // 4. Append the new <div> to an existing element in the document
+    document.body.appendChild(newDiv); // Adds the new <div> as the last child of the <body>
+  }
+
+  // Add an event listener to the button
+  button.addEventListener("click", handleClick);
+
+  return (
+    <div className="App">
+      <Card />
+    </div>
+  );
+}
---
+++ src/Card.jsx
@@ -0,0 +1,22 @@
+export default () => (
+  <div className="birthdayCard">
+    <div className="cardFront">
+      <h3 className="happy">HAPPY BIRTHDAY! -Love Quinn</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside px-20">
+      <h3 className="back">HAPPY BIRTHDAY MAMA!</h3>
+      <p>Dear Mama,</p>
+      <p>
+        Happy birthday! I hope you'll enjoy today as well as many more birthdays
+        to come and this card!
+      </p>
+      <p className="name"></p>
+    </div>
+  </div>
+);
